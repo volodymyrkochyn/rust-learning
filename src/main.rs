6de41987f3ae1e34f14e05e2163886158b953032
Volodymyr Kochyn (Global Logic) <volodymyr.kochyn@globallogic.com>
2025-10-13 23:14:38 +0300

    Initialize logger

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use flexi_logger::{FileSpec, Logger};
+use log::*;
+use dirs;
+
+fn init_logger() -> Result<flexi_logger::LoggerHandle, flexi_logger::FlexiLoggerError> {
+    let mut dir = dirs::home_dir().unwrap();
+    dir.push("logs");
+    let logger = Logger::try_with_str("info")?
+        .log_to_file(FileSpec::default().directory(dir))         // write logs to file
+        .duplicate_to_stderr(flexi_logger::Duplicate::Warn)     // print warnings and errors also to the console
+        .start()?;
+    Ok(logger)
+}
+
 fn main() {
-    println!("Hello, rust-leaning!");
+    init_logger().unwrap();
+
+    info!("This goes to logs/log_0.log");
+    warn!("This also goes to logs/log_0.log");
 }
